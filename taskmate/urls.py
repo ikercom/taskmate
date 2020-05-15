841c50924356ab22e77fe2c9c1a2e2d5f108bba0
Iker Sirius <ikerxto@gmail.com>
2020-05-15 18:23:01 -0500

    update

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -4,8 +4,11 @@ from todolist_app import views as todolist_views
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
     path('', todolist_views.index, name='index'),
+    path('/', todolist_views.index, name='index'),
+
     path('todolist/', include('todolist_app.urls')),
     path('account/', include('users_app.urls')),
     path('contact', todolist_views.contact, name='contact'),
